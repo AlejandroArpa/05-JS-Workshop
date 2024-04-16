399cc95fd5cda3ce8bb3350b3882d173a354e2bb
Alejandro Ramirez <alejorp195@hotmail.com>
2024-04-16 17:14:45 -0500

    Add challenge 2 and modify challenge 1

--- seccion1/Reto1.js
+++ seccion1/Reto1.js
@@ -1,26 +1,20 @@
 
 const costPerDay = 120000;
-let flag = 0;
 const destination = prompt("Ingrese el destino por favor:");
 let days , budget, costTravel;
 
-while(flag === 0){
     
 
-    days = prompt("Ingrese la cantidad de dias del viaje");
-    budget = prompt("Ingrese presupuesto del viaje");   
-    costTravel = costPerDay * days;
-    if( budget >= costTravel ){
-        alert("Dinero suficiente");
-        console.log("Dinero suficiente")
-        flag = 1;
-    }
-    else{
-        alert("Dinero insuficiente ");       
-        console.log("Dinero insuficiente")
-        
-    }
+days = prompt("Ingrese la cantidad de dias del viaje");
+budget = prompt("Ingrese presupuesto del viaje");   
+costTravel = costPerDay * days;
+if( budget >= costTravel ){
+    alert("Dinero suficiente");
+    console.log("Dinero suficiente")
+    flag = 1;
 }
-
-
-
+else{
+    alert("Dinero insuficiente ");       
+    console.log("Dinero insuficiente")
+    
+}
